769dea49bd15442d6978c40e0e8fa42d65194a8e
CaigCaig <caig@inbox.ru>
2023-07-22 11:41:55 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string name;
+    cout << "Введите имя: ";
+    cin >> name;
+    std::cout << "Здравствуйте, " << name << "!\n";
+    system("pause");
+}
